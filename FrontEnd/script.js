9c307590d1ce8ee4488ec6880ffdfec1e929de4f
PauloMalavasi <paulomalavasi44@gmail.com>
2025-03-26 20:18:05 -0300

    Add files via upload

---
+++ FrontEnd/script.js
@@ -0,0 +1,38 @@
+// 24
+alert("Ola, Mundo")
+
+// 25
+console.log("Meu primeiro script em JavaScript")
+
+// 26
+let num1 = 29
+let num2 = 17
+
+console.log(num1 + num2)
+
+// 27
+let name = prompt("Digite seu nome: ")
+
+alert("Bem vindo! " + name)
+
+// 29
+let numBoolean= Boolean()
+
+console.log(numBoolean)
+
+// 30
+
+console.log (typeof(num1))
+
+// 31
+
+let n1 = Number(prompt("Digite um número: "))
+let n2 = Number(prompt("Digite outro número: "))
+let soma = n1 + n2
+alert("A soma é: " + soma)
+
+
+
+
+
+
